24f01ccecdbe2373815c662e4cf8e19ae6928bd8
icdcom <icdcom@hotmail.com>
2016-10-16 12:14:42 +0800

    Initial state

---
+++ BattaTank/Source/BattaTank/BattaTankGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattaTank.h"
+#include "BattaTankGameMode.h"
+
+
+
+
---
+++ BattaTank/Source/BattaTank/BattaTankGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BattaTankGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTATANK_API ABattaTankGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
