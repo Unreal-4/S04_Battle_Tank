18939b38d5245edc71ca2d43e384086c982e35d5
icdcom <icdcom@hotmail.com>
2017-01-01 23:14:27 +0800

    Regenerated VS project

---
+++ BattaTank/Source/BattaTank/BattaTankGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BattaTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTATANK_API ABattaTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
